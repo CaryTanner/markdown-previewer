bbca7df7b7b3f60b4b8aa8752886d96bf5a15139
CaryTanner <cary.landon@gmail.com>
2020-09-15 14:22:42 +0200

    import marked & set up SCSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import marked from 'react-marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './index.css';
+import {explainerText} from './explainerText'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorInput: explainerText,
+    
+    };
+ this.handleChange = this.handleChange.bind(this); 
+   
+  }
+ handleChange(event) { 
+   this.setState({editorInput: event.target.value               
+                 });      
+ }
+  
+  render() {
+    return (
+      <div>
+        <h1>_Markdown Previewer_</h1>
+        <h3>Enter some text to be converted</h3>  
+        <Editor value={this.state.editorInput} onChange={this.handleChange} />
+        <h3>Here is what your markdown looks like in HTML</h3> 
+        <Previewer output={this.state.editorInput} />
+        
+      </div>  
+    );
+  }
+};
+
+function Editor(props){
+  return <textarea id="editor" value={props.value} onChange={props.onChange}></textarea>
 }
+
+function Previewer(props){
+  return <p id="preview" dangerouslySetInnerHTML={{__html: marked(props.output)}}></p>
+}
+
+
 
 export default App;
---
+++ src/explainerText.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+export const explainerText = `Some samples [from Github Guides](https://guides.github.com/features/mastering-markdown/)
+
+# Headers
+
+# This is an \`<h1>\` tag
+## This is an \`<h2>\` tag
+###### This is an \`<h6>\` tag
+
+## Some other h2 :-) 
+
+# Emphasis
+
+*This text will be italic*
+
+_This will also be italic_
+
+**This text will be bold**
+
+__This will also be bold__
+
+_You **can** combine them_
+
+# Lists
+
+## Unordered
+
+* Item 1
+* Item 2
+  * Item 2a
+  * Item 2b
+
+## Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+   1. Item 3a
+   1. Item 3b
+
+# Images
+
+Format: ![Alt Text](url)
+
+# Links
+
+http://github.com - automatic!
+\`[GitHub](http://github.com)\`
+
+# Code 
+
+\`\`\`
+      if (isAwesome){
+          return true
+          }
+
+\`\`\`
+
+
+# Blockquotes
+
+As Kanye West said:
+
+> We're living the future so
+> the present is our past.
+
+# Inline code
+
+I think you should use an
+\`<div>\` element here instead.
+
+# Images
+
+Format: \`![Alt Text](url)\`
+
+\`![A tear drop](https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/tear.png)\`
+
+![A tear drop](https://s3-us-west-2.amazonaws.com/s.cdpn.io/12005/tear.png)
+
+`;
